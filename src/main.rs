b931579e04a8a82dffe64eaf895d939c13074725
Tegan Jain <teganjain527@gmail.com>
2026-08-12 22:33:21 +0530

    feat: add live mode support

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+use clap::Parser;
+
+use crate::utils::clear_screen;
+
 mod reports;
 mod utils;
+
+#[derive(Parser)]
+#[command(about = "System Monitor for Linux")]
+#[command(next_line_help = true)]
+struct Cli {
+    #[arg(
+        short = 'T',
+        value_name = "interval",
+        help = "time interval to refresh for live mode"
+    )]
+    time_period: Option<u64>,
+}
 
 pub fn print_report() -> Result<(), procfs::ProcError> {
     println!("==============================================================");
@@ -32,8 +48,21 @@ pub fn print_report() -> Result<(), procfs::ProcError> {
 }
 
 fn main() {
-    if let Err(err) = print_report() {
-        eprintln!("Something went wrong");
-        eprintln!("{}", err);
-    };
+    let args = Cli::parse();
+
+    if let Some(interval) = args.time_period {
+        loop {
+            if let Err(err) = print_report() {
+                eprintln!("Something went wrong");
+                eprintln!("{}", err);
+            };
+            std::thread::sleep(std::time::Duration::from_secs(interval));
+            clear_screen();
+        }
+    } else {
+        if let Err(err) = print_report() {
+            eprintln!("Something went wrong");
+            eprintln!("{}", err);
+        };
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -9,3 +9,12 @@ pub fn print_header(header: &str) {
     println!("{}", header);
     print_break();
 }
+
+pub fn clear_screen() {
+    // Source - https://stackoverflow.com/a/66911945
+    // Posted by Dan Charousek
+    // Retrieved 2026-08-12, License - CC BY-SA 4.0
+
+    print!("{esc}c", esc = 27 as char);
+    // print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+}
